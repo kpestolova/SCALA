c98d9bbd4be47d36182ec2dcef364af29d851c9b
kpestolova <pestolova01@gmail.com>
2020-09-07 19:03:09 +0300

    HomeWork (2)

--- src/main/scala/HMCookBook.scala
+++ src/main/scala/HMCookBook.scala
@@ -1,6 +1,7 @@
 import java.io.FileWriter
 import java.nio.charset.CodingErrorAction
 import scala.io.Codec
+import scala.util.matching.Regex
 
 object HMCookBook extends App {
 
@@ -20,11 +21,18 @@ object HMCookBook extends App {
     myLines
   }
 
-  def processSeq(mySeq:Seq[String])= {
-    var newSeq: Seq[String] = Seq()
-    println(newSeq.indexOf(newSeq.find(_ contains("    "))))
-    newSeq
-  }
+
+    def processSeq(mySeq: Seq[String]): Seq[String] = {
+
+      val FirstIndex = mySeq.indexOf("(All measurements should be level.)")
+      val LastIndex = mySeq.indexOf("WALTER BAKER & CO., Ltd.")
+      val slicedText = mySeq.slice(FirstIndex,LastIndex)
+      var newSeq: Seq[String] = slicedText.filter(s => s.startsWith("    ") || s == s.toUpperCase)
+      newSeq
+    }
+// I tried to find indexes of filtered lines so I could extend only the needed once, but my code didn't work
+  //println(myLines.indexOf(myLines.find(_ contains("    "))))
+  // Why does it return a string with "    " but doesn't find it's index?
 
   def saveSeq(destName:String, mySeq:Seq[String]) = {
     println(s"Saving my Sequence to file $destName")
